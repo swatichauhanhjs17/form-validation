760f95a7c6f83d759eb8245a9dd15527aeedf18b
swati <swatichauhanhjs17@gmail.com>
2020-05-13 19:18:17 +0530

    input not changing

--- src/cljs/form_validation/core.cljs
+++ src/cljs/form_validation/core.cljs
@@ -10,67 +10,30 @@
 ;; -------------------------
 ;; Routes
 
-
 (def my-name (reagent/atom nil))
 (def my-number (reagent/atom nil))
 
-(defn submit-button [string-value]
+(defn form-input []
+  (let [ string-value (reagent/atom {:name "name" :number "12345"})]
+  [:div
+   [:p "Name: "    [:input {:type "text"
+                           :value (get @string-value :name)
+                           :on-change #(swap! string-value assoc :name(-> % .-target .-value))}]]
 
-  (js/console.log " submit-button rendered")
-  (fn []
-    [:input {:type "submit",
-             :value "Submit"
-             :on-click  #(do (reset!  my-name   (get @string-value :name))
-                             (reset!  my-number  (get @string-value :number)) )}
-     ]))
-
-
-(defn form-components []
-  (let [string-value (reagent/atom {:name "name" :number "12345"})]
-
-    (defn name-text-input [string-value]
-      (js/console.log " text-input rendered")
-      [:input {:type "text"
-               :value (get @string-value :name)
-               :on-change #(swap! string-value assoc :name(-> % .-target .-value))}])
-
-    (defn name-text []
-      (js/console.log " change-text rendered")
-        (fn []
-          [:div
-           [:p "Name: " [name-text-input string-value]
-            ]]))
-
-    (defn number-text-input [string-value]
-      (js/console.log " text-input rendered")
-      [:input {:type "text"
-               :value (get @string-value :number)
-               :on-change #(swap! string-value assoc :number(-> % .-target .-value))}])
-
-    (defn number-text []
-
-        (js/console.log " change-text rendered")
-        (fn []
-          [:div
-           [:p "Number: " [number-text-input string-value]
-
-            ]])))
-
-
-
-[submit-button string-value]
-    )
-
-
-
-
+    [:p "Number"    [:input {:type "text"
+                        :value (get @string-value :number)
+                        :on-change #(swap! string-value assoc :number (-> % .-target .-value))}]]
+                     [:input {:type "submit" ,
+                            :value "Submit"
+                            :on-click  #(do (reset!  my-name   (get @string-value :name))
+                                            (reset!  my-number  (get @string-value :number)) )}]] ))
 
 (defn validation [my-name my-number]
-
-    (js/console.log " validation rendered")
     (fn []
-      (if (< 15 my-name)  "valid"  "not-valid")
-      (if (< 11 my-number))  "valid" "not-valid"))
+      (if (< 15 (count my-name) )  "valid"  "not-valid")
+      (if (< 11 (count my-number)) "valid" "not-valid")))
+
+
 (def router
   (reitit/router
     ["/" :index]))
@@ -88,9 +51,8 @@
   (fn []
     [:span.main
      [:h1 "Welcome to form-validation"]
-
-    [form-components]
-     [validation my-name my-number]]
+    [form-input]
+]
     ))
 
 
